45345aa8aafcde027eace600d907476abc7001fb
QuietMisdreavus <bryan@icesoldier.me>
2017-01-03 14:45:47 -0600

    give the pokemon items too

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,14 @@ extern crate rustc_serialize;
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
 
-const CSV_PATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/pokedex/pokedex/data/csv");
+use rand::distributions::{Range, Sample};
 
-struct Env {
-    pub pokemon_names: HashMap<u32, String>,
-    pub pokemon_moves: HashMap<u32, HashSet<u32>>,
-    pub move_names: HashMap<u32, String>,
-}
+const CSV_PATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/pokedex/pokedex/data/csv");
 
 fn main() {
     let env = Env::load();
     let mut rng = rand::thread_rng();
+    let mut range = Range::new(0u8, 10u8);
 
     for _ in 0..10 {
         let (ref id, ref name) = rand::sample(&mut rng, &env.pokemon_names, 1)[0];
@@ -30,8 +27,21 @@ fn main() {
             }
             print!("{}", env.move_names.get(move_id).unwrap());
         }
+
+        if range.sample(&mut rng) < 7 {
+            let item = rand::sample(&mut rng, &env.items, 1)[0];
+            print!(", holding {}", item);
+        }
+
         println!("");
     }
+}
+
+struct Env {
+    pub pokemon_names: HashMap<u32, String>,
+    pub pokemon_moves: HashMap<u32, Vec<u32>>,
+    pub move_names: HashMap<u32, String>,
+    pub items: Vec<String>,
 }
 
 impl Env {
@@ -51,8 +61,8 @@ impl Env {
                                    .has_headers(true);
         let mut pokemon_moves = HashMap::new();
         for m_row in move_rdr.decode::<PokemonMove>().filter_map(|m| m.ok()).filter(|m| m.group_id == 16) {
-            let moves = pokemon_moves.entry(m_row.pokemon_id).or_insert(HashSet::new());
-            moves.insert(m_row.move_id);
+            let moves = pokemon_moves.entry(m_row.pokemon_id).or_insert(Vec::new());
+            moves.push(m_row.move_id);
         }
 
         let mut move_name_rdr = csv::Reader::from_file(csv_dir.join("move_names.csv"))
@@ -63,10 +73,33 @@ impl Env {
             move_names.insert(n_row.move_id, n_row.name);
         }
 
+        //
+        let mut item_rdr = csv::Reader::from_file(csv_dir.join("items.csv"))
+                                       .unwrap()
+                                       .has_headers(true);
+        let mut item_name_rdr = csv::Reader::from_file(csv_dir.join("item_names.csv"))
+                                            .unwrap()
+                                            .has_headers(true);
+        let mut items = HashSet::new();
+        let mut item_names = Vec::new();
+
+        for item_row in item_rdr.decode::<Item>().filter_map(|i| i.ok()) {
+            if [3,4,5,6,7,12,13,15,17,18,19].contains(&item_row.category) {
+                items.insert(item_row.item_id);
+            }
+        }
+
+        for (item_id, lang_id, name) in item_name_rdr.decode::<(u32, u32, String)>().filter_map(|n| n.ok()) {
+            if lang_id == 9 && items.contains(&item_id) {
+                item_names.push(name);
+            }
+        }
+
         Env {
             pokemon_names: pokemon_names,
             pokemon_moves: pokemon_moves,
             move_names: move_names,
+            items: item_names,
         }
     }
 }
@@ -94,4 +127,14 @@ struct MoveName {
     move_id: u32,
     lang_id: u32,
     name: String,
+}
+
+#[derive(RustcDecodable)]
+struct Item {
+    item_id: u32,
+    _identifier: String,
+    category: u32,
+    _cost: u32,
+    _fling_power: Option<u32>,
+    _fling_effect_id: Option<u32>,
 }
